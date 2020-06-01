9b10ae7950298a68150bac1f8b49cf52f105e965
Theodore Butts <theodorebutts@gmail.com>
2020-06-01 13:42:45 +1000

    forgot to remove blur test on prev commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,62 +1,73 @@
 const m = moment();
 var topTime = document.querySelector("#currentDay");
-var list = JSON.parse(localStorage.getItem('newUserInput')) || {};
+var list = JSON.parse(localStorage.getItem("newUserInput")) || {};
 
+$(document).ready(function() {
 // Set Top Time
-topTime.textContent = m.format("dddd MMM DD, YYYY")
+topTime.textContent = m.format("dddd MMM DD, YYYY");
 
 // User Input
-$(".list-group").on('click', "ul", function () {
+$(".list-group").on("click", "ul", function () {
     // get current text
     var currentInput = $(this).text().trim();
+    
     // create new input element
     var UserInput = $("<input>")
         .addClass("inputarea")
         .attr("type", "text")
         .val(currentInput);
-    $(this).replaceWith(UserInput);
+        $(this).replaceWith(UserInput);
 });
 
-
 // Save button
-$('.saveBtn').on('click', function (event) {
+$(".saveBtn").on("click", function (event) {
     event.preventDefault();
+   
     var newUserInput = $(this)
+        .trigger("focus")
         .siblings(".textarea")
         .children()
         .val();
-    var activeHour = $(this)
+    
+        var activeHour = $(this)
         .siblings(".hour")
-        .attr('data-hour');
-    list[activeHour] = newUserInput
-
-    localStorage.setItem('newUserInput', JSON.stringify(list));
-
+        .attr("data-hour");
+    
+        list[activeHour] = newUserInput
+    
+    localStorage.setItem("newUserInput", JSON.stringify(list));
 });
 
 // Recall from storage
 function loadTasks(list) {
     for (key in list) {
-        var toDoItem = $('.hour-' + key + "-color");
+        var toDoItem = $(".hour-" + key + "-color");
         toDoItem.children().text(list[key]);
         checkDates();
-    }
-}
+    };
+};
 
 // Time Color Change
 function checkDates() {
     for (var i = 9; i < 18; i++) {
         var currentTime = $(".hour-" + i).data("hour");
-        var momentInt = parseInt(m.format("HH"))
-        $(".hour-" + i + "-color").removeClass("past present future")
-        if (currentTime < momentInt) {
-            $(".hour-" + i + "-color").addClass("past")
-        } else if (currentTime > momentInt) {
-            $(".hour-" + i + "-color").addClass("future")
-        } else if (currentTime === momentInt) {
-            $(".hour-" + i + "-color").addClass("present")
-        }
-    }
-}
 
+        var momentInt = parseInt(m.format("HH"));
+
+        $(".hour-" + i + "-color").removeClass("past present future");
+
+        if (currentTime < momentInt) {
+            $(".hour-" + i + "-color").addClass("past");
+        } else if (currentTime > momentInt) {
+            $(".hour-" + i + "-color").addClass("future");
+        } else if (currentTime === momentInt) {
+            $(".hour-" + i + "-color").addClass("present");
+        };
+    };
+};
+
+checkDates();
 loadTasks(list);
+
+
+});
