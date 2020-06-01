b18295af789d5d5f66223c7d78d04e15920fff3b
Theodore Butts <theodorebutts@gmail.com>
2020-05-31 19:55:16 -0600

    html to better fit use, add task functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,46 @@
-var tasks = {};
+const m = moment();
+var topTime = document.querySelector("#currentDay");
+var list = JSON.parse(localStorage.getItem('newUserInput')) || {};
 
-var currentDay = function(now) {
-    now = moment().format("dddd, MMMM Do YYYY, HH:MM").toString();
-   
-    $("#currentDay").text(now);
-    console.log(now);
-   
+// Set Top Time
+topTime.textContent = m.format("dddd MMM DD, YYYY")
+
+// User Input
+$(".list-group").on('click', "ul", function () {
+    // get current text
+    var currentInput = $(this).text().trim();
+    // create new input element
+    var UserInput = $("<input>")
+        .addClass("inputarea")
+        .attr("type", "text")
+        .val(currentInput);
+    $(this).replaceWith(UserInput);
+});
+
+
+// Save button
+
+$('.saveBtn').on('click', function (event) {
+    event.preventDefault();
+    var newUserInput = $(this)
+        .siblings(".textarea")
+        .children()
+        .val();
+    var activeHour = $(this)
+        .siblings(".hour")
+        .attr('data-hour');
+    list[activeHour] = newUserInput
+
+    localStorage.setItem('newUserInput', JSON.stringify(list));
+
+});
+
+// Recall from storage
+function activateLocal(list) {
+    for (key in list) {
+        var toDoItem = $('.hour-' + key + "-color");
+        toDoItem.children().text(list[key]);
+        checkDates();
+    }
 }
 
-currentDay();
